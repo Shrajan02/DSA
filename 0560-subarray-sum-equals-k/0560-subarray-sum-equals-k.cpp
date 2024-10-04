0979ddf8d1b5be8f24115385b5e487e9c4b546e2
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-10-04 15:08:27 +0530

    Time: 100 ms (41.03%), Space: 47.6 MB (41.14%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-
         int count = 0, preSum = 0;
         int n = nums.size();
         map<int, int> hash; 
 
-        hash[0] = 1; // Edge case: here subarray starts from index 0
+        hash[0] = 1; // considering addition with singleton subarrays
 
         for (int i = 0; i < n; i++) {
-            preSum += nums[i]; // Update prefix sum
+            preSum += nums[i]; 
 
-            // Check if (preSum - k) exists in the map
-            if (hash.find(preSum - k) != hash.end()) {
-                count += hash[preSum - k]; // Add the frequency of (preSum - k)
+            // Check if (preSum - k) exists in hashmap
+            int rem = preSum - k;
+            if (hash.find(rem) != hash.end()) {
+                count += hash[rem]; 
             }
 
-            // Store the frequency of the current prefix sum
-            hash[preSum]++;
+            hash[preSum]++; // Updating hashmap
         }
 
         return count;
