ae010e2136bdc102ee114e7417a3ae3f964d8562
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-10-04 16:02:57 +0530

    Time: 115 ms (29.88%), Space: 53.8 MB (12.58%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,16 +3,14 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int count = 0, preSum = 0;
         int n = nums.size();
-        map<int, int> hash;  // <preSum, count>
+        map<int, int> hash = {{0, 1}}; // <preSum, count>
 
-        hash[0] = 1; // setting 0 in the hashmap
-        for (int i = 0; i < n; i++) {
-            preSum += nums[i]; 
-            
+        for (auto i : nums) {
+            preSum += i; 
+        
             // Check if (preSum - k) exists in hashmap
             int rem = preSum - k;
             count += hash[rem]; 
-            
             hash[preSum]++; // Updating hashmap
         }
         return count;
