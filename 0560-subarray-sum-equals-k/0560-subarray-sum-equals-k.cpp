35457708c014d1f866524889c8d27f512f4c548a
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-10-04 15:57:29 +0530

    Time: 107 ms (35.84%), Space: 53.9 MB (7.86%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -5,18 +5,16 @@ public:
         int n = nums.size();
         map<int, int> hash;  // <preSum, count>
 
-        hash[0] = 1; // considering addition with singleton subarrays
-
+        hash[0] = 1; // setting 0 in the hashmap
         for (int i = 0; i < n; i++) {
             preSum += nums[i]; 
-        
+            
             // Check if (preSum - k) exists in hashmap
             int rem = preSum - k;
             count += hash[rem]; 
-        
+            
             hash[preSum]++; // Updating hashmap
         }
-
         return count;
     }
 };
