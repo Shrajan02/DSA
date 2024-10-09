0bbb81e2a5ecc12a52d318775cc4285ec9da06fc
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-10-09 21:59:11 +0530

    Time: 87 ms (12.28%), Space: 70.6 MB (21.29%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,8 +4,9 @@ public:
         // Kadane's Algorithm: O(n)
         int currSum = 0, maxSum = nums[0];
         for (int i = 0; i < nums.size(); i++) {
-            currSum += nums[i];
-            maxSum = max(maxSum, currSum);
+            currSum += nums[i];   // Add element to the current subarray sum
+            maxSum = max(maxSum, currSum);   // Update maximum sum
+            // Reset current sum to 0 when sum is negative
             if (currSum < 0) {
                 currSum = 0;
             }
