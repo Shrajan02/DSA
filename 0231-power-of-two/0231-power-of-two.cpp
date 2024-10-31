25af8f184487dfcbe86e19ade8ba752590005df0
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-10-31 21:34:29 +0530

    Time: 0 ms (100%), Space: 7.5 MB (72.49%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n == 0) return false;
-        return (1LL * n & (1LL * n -1)) ? false: true;
+        return n > 0 && (1LL * n & (1LL * n - 1)) == 0;
     }
 };
