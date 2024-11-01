de35b79e67589abb6d000806ef1aa91899211f1f
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-11-02 01:24:03 +0530

    Time: 23 ms (74.74%), Space: 42.8 MB (31.24%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-        int n = s.length(), count = 0;
+        int n = s.length(), count = 1;
         string ans = "";
+        ans += s[0];
         char letter = s[0];
-        for (int i = 0; i < n; i++) {
+
+        for (int i = 1; i < n; i++) {
             if (letter != s[i]) {
                 count = 1;
                 letter = s[i];
