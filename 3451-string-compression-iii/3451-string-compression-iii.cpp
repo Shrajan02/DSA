36d8fe85a3693d32f9e22ff7b887d1b2e15e9d33
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-11-04 21:14:40 +0530

    Time: 11 ms (94.02%), Space: 28.3 MB (76.84%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.cpp
+++ 3451-string-compression-iii/3451-string-compression-iii.cpp
@@ -6,7 +6,7 @@ public:
         for (int i = 1; i < n; i++) {
             // does not match the previous OR exceeds count
             if (word[i] != word[i - 1] || count >= 9) {
-                comp += to_string(count);
+                comp += ('0'+ count); 
                 comp += word[i - 1];
                 count = 1;
                 
