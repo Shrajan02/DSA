4bd0153e3272798a3c221bef050bbd2eb98fdb65
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-11-06 00:06:27 +0530

    Time: 0 ms (100%), Space: 12.2 MB (67.52%) - LeetHub

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int minChanges(string s) {
-        int count = 0;
-        for (int i = 0; i < s.size() - 1; i += 2) {
+        int count = 0, n = s.size();
+        for (int i = 0; i < n-1; i += 2) {
             if (s[i] != s[i + 1]) {
                 count++;
             }
