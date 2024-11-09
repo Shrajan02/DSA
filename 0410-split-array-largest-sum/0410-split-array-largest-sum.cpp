ff0018bf3c94b1b8f90d1d2920caa35c93cca98e
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-11-09 19:31:18 +0530

    Time: 0 ms (100%), Space: 10 MB (10.4%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool minSplit(vector<int> nums, int maxAllowedSum, int maxSplits) {
+        int split = 1, currSum = 0;  // Initially, the 1st split has no elements: zero sum
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > maxAllowedSum) {
+                return false; // If any element is greater than sum, impossible 
+            }
+            if (currSum + nums[i] <= maxAllowedSum) {
+                currSum += nums[i];
+            }
+            else {
+                split++;
+                currSum = nums[i];
+            }
+        }
+
+        return (split <= maxSplits);
+    }
+    
+    
+    int splitArray(vector<int>& nums, int k) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int maxi = *max_element(nums.begin(), nums.end());
+
+        // Binary Search Approach
+        int low = maxi, high = sum, maxSum = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            bool possible = minSplit(nums, mid, k);
+            if (possible) {
+                maxSum = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return maxSum;
+    }
+};
