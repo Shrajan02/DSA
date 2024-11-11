a7c2f8a9710ec8f5cc590eb7668ba6a259cbdad4
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-11-11 23:41:06 +0530

    Time: 3 ms (79.27%), Space: 14.3 MB (43.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> hashMap;
         int n = nums.size();
-        // 2-pass Hash Table
+        unordered_map<int, int> hashmap;
         for (int i = 0; i < n; i++) {
-            hashMap[nums[i]] = i;
-        }
-
-        for(int i = 0; i < n; i++) {
             int complement = target - nums[i];
-            if(hashMap.find(complement) != hashMap.end() && hashMap[complement] != i)
-                return {hashMap[complement], i};
+            if(hashmap.find(complement) != hashmap.end()) {
+                return {hashmap[complement], i};
+            }
+            hashmap[nums[i]] = i;
         }
         return {};
     }
