89d1b628fcd71ad4c6493d58bfb2e36ba74a0a69
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-11-13 01:50:52 +0530

    Time: 0 ms (100%), Space: 12.1 MB (92.48%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size();  // rows
-        int m = matrix[0].size();  // columns
-        int low = 0, high = n-1;
+        int n = matrix.size();    // rows
+        int m = matrix[0].size(); // columns
+        int low = 0, high = n * m - 1;
 
         while (low <= high) {
-            int midRow = low + ((high - low) >> 1);
-            // perform BS within this 1D array 
-            if (matrix[midRow][0] <= target && matrix[midRow][m-1] >= target) {  
-                int left = 0, right = m-1;
-                while(left <= right) {
-                    int midCol = left + ((right - left) >> 1);
-                    if (matrix[midRow][midCol] > target) {
-                        right = midCol - 1;
-                    }
-                    else if (matrix[midRow][midCol] < target) {
-                        left = midCol + 1;
-                    }
-                    else {
-                        return true;
-                    }
-                }
-                return false;
+            int mid = low + ((high - low) >> 1);
+            int row = mid / m, col = mid % m; // flattening 2D array to 1D
+            if (matrix[row][col] > target) {
+                high = mid - 1;
+            } else if (matrix[row][col] < target) {
+                low = mid + 1;
+            } else {
+                return true;
             }
-            else if (matrix[midRow][0] > target) {
-                high = midRow - 1;
-            }
-            else {
-                low = midRow + 1;
-            }
-
         }
         return false;
     }
