104505512c266815d9f8f0744975a1b58bbf40c0
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-11-12 14:03:01 +0530

    Time: 0 ms (100%), Space: 95.4 MB (40.82%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -3,50 +3,40 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int m = nums1.size(), n = nums2.size();
 
-        if (m == 0 && n == 0)
-            return (double)(0);
+        if (m == 0 && n == 0) return (double)(0);
+        vector<int> merge;
 
-        // Checking for medians using 2-pointers(without merging)
-        int total = m + n;
-        int mid1 = total / 2 - 1, mid2 = total / 2;
-        int median1 = -1, median2 = -1;
-        int i = 0, j = 0, count = 0;
+        // Merging arrays using 2-pointers
+        int i = 0, j = 0;
         while (i < m && j < n) {
             if (nums1[i] < nums2[j]) {
-                if (count == mid1) median1 = nums1[i];
-                if (count == mid2) median2 = nums1[i];
-                count++;
+                merge.push_back(nums1[i]);
                 i++;
             }
-            else {
-                if (count == mid1) median1 = nums2[j];
-                if (count == mid2) median2 = nums2[j];
-                count++;
+            else if (nums1[i] > nums2[j]) {
+                merge.push_back(nums2[j]);
                 j++;
             }
+            else {
+                merge.push_back(nums1[i++]);
+                merge.push_back(nums2[j++]);
+            }
         }
-
         while (i < m) {
-            if (count == mid1) median1 = nums1[i];
-            if (count == mid2) median2 = nums1[i];
-            count++;
-            i++;
+            merge.push_back(nums1[i++]);
         }
-
         while (j < n) {
-            if (count == mid1) median1 = nums2[j];
-            if (count == mid2) median2 = nums2[j];
-            count++;
-            j++;      
+            merge.push_back(nums2[j++]);
         }
 
+        int median = (m + n) >> 1;  
         // Odd length case
-        if (total % 2 == 1) {
-            return (double)median2;
+        if ((m + n) % 2 == 1) {
+            return (double)(merge[median]);
         }
         // Even length case
         else {
-            return (double)((median1 + median2) / 2.0);
+            return (merge[median-1] + merge[median]) / 2.0; 
         }
     }
 };
