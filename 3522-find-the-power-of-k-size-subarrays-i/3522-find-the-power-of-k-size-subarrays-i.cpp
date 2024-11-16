55bb722a0aed2765fabbd6c267ec469ccc6fd139
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-11-16 23:29:35 +0530

    Time: 0 ms (100%), Space: 32.9 MB (91.28%) - LeetHub

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
@@ -1,18 +1,19 @@
 class Solution {
-public:
+private:
     int power(vector<int>& nums, int start, int k) {
-        // Check for sorted consecutive, then max 
+        // Check for sorted consecutive, then max
         for (int i = start; i < start + k - 1; i++) {
-            if (nums[i] != nums[i+1] - 1)
+            if (nums[i] != nums[i + 1] - 1)
                 return -1;
         }
         return nums[start + k - 1];
     }
-    
+
+public:
     vector<int> resultsArray(vector<int>& nums, int k) {
         int n = nums.size();
         vector<int> results(n - k + 1);
-        for (int i = 0; i <= n-k; i++) {
+        for (int i = 0; i <= n - k; i++) {
             results[i] = power(nums, i, k);
         }
         return results;
