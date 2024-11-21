c219af23c1b4e322ac8d6a0247a0ea99eb09c97b
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-11-21 21:14:40 +0530

    Time: 4 ms (56.11%), Space: 10.1 MB (17.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,13 +1,25 @@
-// TC: O(n*logn)
-// SC: O(1)
+// TC: O(n) ; n -> length of string 't'
+// SC: O(n)
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) return false;
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if (s != t) {
+        if (s.length() != t.length())
             return false;
+
+        unordered_map<char, int> hashMapS, hashMapT;
+        for (int i = 0; i < s.length(); i++) {
+            hashMapS[s[i]]++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            hashMapT[t[i]]++;
+        }
+
+        int i = 0, n = t.length();
+        while (i < n) {
+            if (hashMapS[t[i]] != hashMapT[t[i]]) {
+                return false;
+            }
+            i++;
         }
         return true;
     }
