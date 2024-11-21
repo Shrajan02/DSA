9f11b034c68486c45f690242776ce79b0f87887c
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-11-21 21:13:34 +0530

    Time: 4 ms (56.11%), Space: 8.9 MB (55.45%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,14 @@
+// TC: O(n*logn)
+// SC: O(1)
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if (s != t) {
+            return false;
+        }
+        return true;
+    }
+};
