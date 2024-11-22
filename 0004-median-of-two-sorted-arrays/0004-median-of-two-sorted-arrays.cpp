d80f53e8c7524ed716d664db7e28332d70780737
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-11-22 13:10:54 +0530

    Time: 0 ms (100%), Space: 94.7 MB (87.36%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -2,41 +2,43 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int m = nums1.size(), n = nums2.size();
+        // Apply BS on minimum array
+        if (m > n)
+            return findMedianSortedArrays(nums2, nums1); 
 
-        if (m == 0 && n == 0) return (double)(0);
-        vector<int> merge;
+        int total = m + n;
+        int left = (total + 1) / 2; // Length of left partition
 
-        // Merging arrays using 2-pointers
-        int i = 0, j = 0;
-        while (i < m && j < n) {
-            if (nums1[i] < nums2[j]) {
-                merge.push_back(nums1[i]);
-                i++;
-            }
-            else if (nums1[i] > nums2[j]) {
-                merge.push_back(nums2[j]);
-                j++;
-            }
-            else {
-                merge.push_back(nums1[i++]);
-                merge.push_back(nums2[j++]);
-            }
-        }
-        while (i < m) {
-            merge.push_back(nums1[i++]);
-        }
-        while (j < n) {
-            merge.push_back(nums2[j++]);
-        }
+        int low = 0, high = m;
+        while (low <= high) {
+            // Partition index for both arrays {mid1 + mid2 == left}
+            int mid1 = low + ((high - low) >> 1); // mid for smaller array
+            int mid2 = left - mid1;               // mid for larger array
 
-        int median = (m + n) >> 1;  
-        // Odd length case
-        if ((m + n) % 2 == 1) {
-            return (double)(merge[median]);
+            // Left & right sides of both partitions
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+            // Checking for out of bounds 
+            if (mid1 < m)
+                r1 = nums1[mid1];
+            if (mid2 < n)
+                r2 = nums2[mid2];
+            if (mid1 - 1 >= 0)
+                l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0)
+                l2 = nums2[mid2 - 1];
+
+            if (max(l1, l2) <= min(r1, r2)) {
+                if (total % 2 == 1)
+                    return max(l1, l2);
+                else
+                    return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
+            } else if (l1 > r2) {
+                high = mid1 - 1;
+            } else {
+                low = mid1 + 1;
+            }
         }
-        // Even length case
-        else {
-            return (merge[median-1] + merge[median]) / 2.0; 
-        }
+        return 0;
     }
 };
