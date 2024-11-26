97b94b4f63d7aa2ca535df04c67c5f97dceda717
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-11-26 23:40:16 +0530

    Time: 0 ms (100%), Space: 8.7 MB (5.31%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -8,11 +8,8 @@ public:
         while (j < t.length()) {
             if (s[i] == t[j]) {
                 i++;
-                j++;
             }
-            else {
-                j++;
-            }
+            j++;
         }
         return (i == s.length());
     }
