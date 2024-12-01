c1afe26031c017b1a0d8092f156f05d213f991bb
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-12-01 21:16:31 +0530

    Time: 1 ms (51.06%), Space: 21.3 MB (49.05%) - LeetHub

--- 0950-x-of-a-kind-in-a-deck-of-cards/0950-x-of-a-kind-in-a-deck-of-cards.cpp
+++ 0950-x-of-a-kind-in-a-deck-of-cards/0950-x-of-a-kind-in-a-deck-of-cards.cpp
@@ -4,6 +4,13 @@
 #include <numeric> // For gcd function
 
 class Solution {
+private:
+    int calculateGCD(int a, int b) {
+        if (b == 0)
+            return a;
+        return calculateGCD(b, a % b);
+    }
+
 public:
     bool hasGroupsSizeX(std::vector<int>& deck) {
         unordered_map<int, int> hashTable;
@@ -16,7 +23,7 @@ public:
         // Find the GCD of all frequencies
         int gcdValue = 0; 
         for (const auto& pair : hashTable) {
-            gcdValue = gcd(gcdValue, pair.second);
+            gcdValue = calculateGCD(gcdValue, pair.second);
             // If at any point the GCD becomes 1, we can stop early
             if (gcdValue == 1) 
                 return false;
