e47acf8d6566343a431bcacb2095c260c7385817
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-12-11 01:36:58 +0530

    Time: 0 ms (100%), Space: 14 MB (27.77%) - LeetHub

---
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,16 @@
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> hashTable;
+        for (int num : arr) {
+            if (hashTable.count(num * 2) || (num % 2 == 0 && hashTable.count(num / 2))) {
+                return true;
+            }
+            hashTable.insert(num);
+        }
+        return false;
+    }
+};
