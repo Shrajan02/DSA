3503c51611aefe96e4cfce34cf0544a82654bf57
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-12-16 02:42:22 +0530

    Time: 13 ms (15.02%), Space: 11.8 MB (25.96%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        // Floyd's Cycle Detection Algo (Tortoise-Hare Algo)
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != NULL && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
