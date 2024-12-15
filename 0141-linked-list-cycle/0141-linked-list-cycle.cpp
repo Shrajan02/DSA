1e9f222bd6c07bc8dd8936bfb28cf32ee94974ef
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-12-16 02:44:17 +0530

    Time: 12 ms (34.05%), Space: 11.9 MB (25.96%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,15 +9,16 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
+        if (head == NULL || head->next == nullptr)
+            return false;
+
         // Floyd's Cycle Detection Algo (Tortoise-Hare Algo)
-        ListNode* slow = head;
-        ListNode* fast = head;
+        ListNode* slow = head, *fast = head;
         while (fast != NULL && fast->next != nullptr) {
             slow = slow->next;
             fast = fast->next->next;
-            if (slow == fast) {
+            if (slow == fast) 
                 return true;
-            }
         }
         return false;
     }
