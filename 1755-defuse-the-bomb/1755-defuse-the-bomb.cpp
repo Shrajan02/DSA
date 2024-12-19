26753b67d47e43f7a0fc72fe6ba00bacf7414587
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-12-19 14:11:40 +0530

    Time: 1 ms (15.33%), Space: 11.7 MB (6.61%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
@@ -2,20 +2,25 @@ class Solution {
 public:
     vector<int> decrypt(vector<int>& code, int k) {
         int n = code.size();
-        vector<int> res(n, 0);
-        
+        vector<int> result(n, 0);
+        if (k == 0)
+            return result;
+
         for (int i = 0; i < n; i++) {
-            if (k > 0) {
-                for (int j = i + 1; j < i + 1 + k; j++) {
-                    res[i] += code[j % n];
-                }
+            int temp = abs(k);
+            int j = i, sum = 0;
+            while (temp--) {
+                if (k > 0) {
+                    j = (j + 1) % n;
+                }  
+                else {
+                    j = (j - 1 + n) % n;
+                }   
+                sum += code[j];
             }
-            else if (k < 0) {
-                for (int j = i - 1; j > i - 1 - abs(k); j--) {
-                    res[i] += code[((j % n) + n) % n];
-                }
-            }
+            result[i] = sum;
         }
-        return res;
+
+        return result;
     }
 };
