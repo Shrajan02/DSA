0f94bf7a94a55fc9e9be0b8027eb7a947f15443b
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2024-12-23 02:50:05 +0530

    Time: 51 ms (28.47%), Space: 75.6 MB (17.52%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -24,8 +24,8 @@ private:
         if (!l1) return l2;
         if (!l2) return l1;
 
-        ListNode* head = new ListNode(0);
-        ListNode* tail = head;  // Tracks end of the merged list
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;  // Tracks end of the merged list
 
         while (l1 && l2) {
             if (l1->val < l2->val) {
@@ -41,7 +41,7 @@ private:
         // Attach remaining nodes
         tail->next = l1 ? l1 : l2; 
 
-        return head->next;
+        return dummy->next;
     }
 
 public:
