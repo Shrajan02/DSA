029c525d67e93d64cc9be609ca891f5b91720bb1
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-01-17 01:22:09 +0530

    Time: 87 ms (5.14%), Space: 92.9 MB (5.14%) - LeetHub

---
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size(), m = nums2.size();
+        unordered_map<int, long> freq; // map to store count of each number
+        
+        for (int i: nums1) {
+            freq[i] += m;  // Each element appears 'm' times in final result
+        }
+        for (int j: nums2) {
+            freq[j] += n;
+        }
+
+        int res = 0;
+        for (auto& [num, count] : freq) {
+            // EVEN: (a ^ a) ^ (a ^ a) = 0
+            // ODD:  (a ^ a) ^  a      = a 
+            if (count % 2 == 1) {
+                res ^= num;  
+            }
+        }
+        return res;
+    }
+};
