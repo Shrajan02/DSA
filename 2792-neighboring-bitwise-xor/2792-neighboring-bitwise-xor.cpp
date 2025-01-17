9f7f6abb9e1ae2f3305634303fa9e5d5cb9dc0bc
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-01-18 01:40:55 +0530

    Time: 4 ms (50%), Space: 263.9 MB (50.63%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
+        // XOR of all elements must be 0 for original array to form a valid cyclic sequence
+        // Commutative: a⊕b = b⊕a
+        // Associative: (a⊕b)⊕c = a⊕(b⊕c)
         int sum = 0;
         for (int i: derived) {
             sum ^= i;
