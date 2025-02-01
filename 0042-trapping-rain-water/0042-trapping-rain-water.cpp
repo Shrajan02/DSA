9f11242aea085f15a038af8f504a35f17acf6c36
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-02-02 02:21:11 +0530

    Time: 0 ms (100%), Space: 26.1 MB (62.68%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,29 +1,35 @@
-// Prefix-Suffix Approach (no stacks)
+// Optimized 2-pointer Approach
 // TC: O(n)
-// SC: O(n)
+// SC: O(1)
 class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
         if (n < 3) return 0; // not possible with less than 3 bars
 
-        vector<int> leftMax(n, 0), rightMax(n, 0);  // boundaries for trapping water
-        // Construct leftMax array
-        leftMax[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            leftMax[i] = max(leftMax[i - 1], height[i]);
-        }
-
-        // Construct rightMax array
-        rightMax[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            rightMax[i] = max(rightMax[i + 1], height[i]);
-        }
-
-        // Calculate trapped water
         int totalWater = 0;
-        for (int i = 0; i < n; i++) {
-            totalWater += min(leftMax[i], rightMax[i]) - height[i];
+        int left = 0, right = n - 1;
+        int maxLeft = 0, maxRight = 0;
+
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= maxLeft) {
+                    maxLeft = height[left];
+                }
+                else {
+                    totalWater += maxLeft - height[left];
+                }
+                left++;
+            }
+            else {
+                if (height[right] >= maxRight) {
+                    maxRight = height[right];
+                }
+                else {
+                    totalWater += maxRight - height[right];
+                }
+                right--;
+            }
         }
         return totalWater;
     }
