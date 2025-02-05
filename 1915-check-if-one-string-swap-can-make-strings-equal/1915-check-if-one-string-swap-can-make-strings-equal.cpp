7db5bc40132a2bd97a43c741a30a78329edc7ad0
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-02-06 00:51:12 +0530

    Time: 0 ms (100%), Space: 8.3 MB (85.97%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,30 +1,32 @@
+// TC: O(n)
+// SC: O(1)
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-        if (s1 == s2) return true;
+        if (s1 == s2)
+            return true;
 
         int n = s1.length();
-        int freq1[26] = {0}, freq2[26] = {0}; // {char->s1, index}
+        int firstIndex = 0, secondIndex = 0;
         int swaps = 0;
 
         for (int i = 0; i < n; i++) {
             char ch1 = s1[i], ch2 = s2[i];
             if (ch1 != ch2) {
                 swaps++;
+                if (swaps > 2) {
+                    return false;
+                }
+                else if (swaps == 1) {
+                    firstIndex = i;
+                }
+                else {
+                    secondIndex = i;
+                }
             }
-            if (swaps > 2) {
-                return false;
-            }
-            freq1[ch1 - 'a']++;
-            freq2[ch2 - 'a']++;
         }
 
-        // check frequencies when swaps <= 2
-        for (int i = 0; i < 26; i++) {
-            if (freq1[i] != freq2[i]) {
-                return false;
-            }
-        }
-        return true;
+        // check if swap is possible
+        return (s1[firstIndex] == s2[secondIndex] && s2[firstIndex] == s1[secondIndex]);
     }
 };
