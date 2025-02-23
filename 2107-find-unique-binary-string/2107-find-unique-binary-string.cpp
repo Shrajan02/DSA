577fa9a441d2b849f7e874b3689e6b78fde31e96
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-02-24 02:26:01 +0530

    Time: 0 ms (100%), Space: 12.6 MB (82.71%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -1,23 +1,20 @@
-// Random Selection approach
-// TC: O(n^2)
-// SC: O(n)
+// Cantor's Diagonal Argument
+// TC: O(n)
+// SC: O(1)
 class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-        unordered_set<int> allPossibleInt; // base-10 integers
-        for (string num : nums) {
-         allPossibleInt.insert(stoi(num, 0, 2));  // (value, position, base)
-        }
-        
-        int ans = stoi(nums[0], 0, 2);  // initial candidate
         int n = nums.size();
-        
-        // using random generation
-        while (allPossibleInt.find(ans) != allPossibleInt.end()) {
-            ans = rand() % (int) pow(2, n);
+        string ans = "";
+        for (int i = 0; i < n; i++) {
+            char curr = nums[i][i];
+            if (curr == '0') {
+                ans += '1';
+            } 
+            else {
+                ans += '0';
+            }
         }
-        
-        // max 16-character binary string
-        return bitset<16>(ans).to_string().substr(16 - n);
+        return ans;
     }
 };
