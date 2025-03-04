7bde34e546b2e65acadd7a4c5c53b4d14515b42a
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-03-05 01:33:12 +0530

    Time: 0 ms (100%), Space: 8 MB (9.2%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,22 +1,15 @@
-// Ternary Representation Approach
-// TC: O(log\3(n))
+// TC: O(1)
 // SC: O(1)
 class Solution {
 public:
     bool checkPowersOfThree(int n) {
-        while (n > 1) {
-            /*
-            Either it is divisible by 3 directly, so divide by 3
-            Or it must be a sum of powers of 3 which includes 3^0, so subtract one and divide by 3
-            [3^2 + 3^1 = 3(3^1 + 3^0)]
-            */
-            if (n % 3 == 0 || (n - 1) % 3 == 0) {
-                n /= 3;
-            }
-            else {
-                return false;
+        // max power to be checked = 3^16
+        for (int i = 16; i >= 0; i--) {
+            int power = std::pow(3, i);
+            if (n - power >= 0) {
+                n -= power;
             }
         }
-        return true;
+        return (n == 0);
     }
 };
