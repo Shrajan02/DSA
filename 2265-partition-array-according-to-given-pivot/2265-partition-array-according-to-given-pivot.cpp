e9efe95b303a3ff46bb632a842b40eb851f0427b
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-03-04 16:36:17 +0530

    Time: 7 ms (59.31%), Space: 127.7 MB (77.38%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,34 +1,30 @@
-// 2-pointer approach(3 pass)
+// 2-pointer approach (1 pass)
 // TC: O(n)
 // SC: O(n)
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         int n = nums.size();
-        vector<int> res(n);
+        vector<int> ans(n);
+        int lessPiv = 0;        // next index of elements < pivot
+        int lessEqualPiv = 0;   // next index of elements <= pivot
+        int greaterPiv = n - 1; // next (from right) index of elements > pivot
 
-        int left = 0; 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] < pivot) {
-                res[left] = nums[i];
-                left++;
+        // l from start, r from end
+        for (int l = 0, r = n - 1; l < n; l++, r--) {
+            if (nums[l] <= pivot) {
+                ans[lessEqualPiv] = pivot;
+                lessEqualPiv++;
+                ans[lessPiv] = nums[l];
+                if (nums[l] != pivot) {
+                    lessPiv++;
+                }
             }
-        }    
-
-        int right = n - 1;
-        for (int j = n - 1; j >= 0; j--) {
-            if (nums[j] > pivot) {
-                res[right] = nums[j];
-                right--;
+            if (nums[r] > pivot) {
+                ans[greaterPiv--] = nums[r];
             }
-        }    
-        
-        // equal elements
-        while(left <= right) {
-            res[left] = pivot;
-            left++;
         }
 
-        return res;
+        return ans;
     }
 };
