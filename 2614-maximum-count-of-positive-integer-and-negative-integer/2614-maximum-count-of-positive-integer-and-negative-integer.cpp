532496f2a981584dde46bcaf5a2104f55bca7b11
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-03-14 22:31:04 +0530

    Time: 0 ms (100%), Space: 21.4 MB (42.32%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,18 +1,47 @@
-// Brute Force approach
-// TC: O(n)
+// Binary Search approach
+// TC: O(log n)
 // SC: O(1)
 class Solution {
-public:
-    int maximumCount(vector<int>& nums) {
-        int pos = 0, neg = 0;
-        for (int num: nums) {
-            if (num > 0) {
-                pos++;
+private:
+    int lowerBound(vector<int>& nums, int n) {
+        int low = 0, high = n - 1; 
+        int ind = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < 0) {
+                low = mid + 1;
             }
-            else if (num < 0) {
-                neg++;
+            else if (nums[mid] >= 0) {
+                high = mid - 1;
+                ind = mid;
             }
         }
-        return max(pos, neg);
+        return ind;
+    }
+
+    int upperBound(vector<int>& nums, int n) {
+        int low = 0, high = n - 1;
+        int ind = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] <= 0) {
+                low = mid + 1;
+            }
+            else if (nums[mid] > 0) {
+                high = mid - 1;
+                ind = mid;
+            }
+        }
+        return ind;
+    }
+
+public:
+    int maximumCount(vector<int>& nums) {
+        int n = nums.size();
+        int firstZero = lowerBound(nums, n);
+        int lastZero = upperBound(nums, n);
+        int neg = firstZero;
+        int pos = n - lastZero;
+        return max(neg, pos);
     }
 };
