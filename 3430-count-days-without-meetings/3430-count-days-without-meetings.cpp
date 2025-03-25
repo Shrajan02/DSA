a8ddd6f0ba520a48c2d5a24574b264b7da77bed1
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-03-25 21:52:06 +0530

    Time: 76 ms (37.54%), Space: 126.4 MB (94.64%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -1,29 +1,24 @@
 // Sorting approach
 // TC: O(n * log(n))
-// SC: O(n)
+// SC: O(1)
 class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
         int n = meetings.size();
         sort(meetings.begin(), meetings.end());  // based on start time
 
-        // merge overlapping meetings
-        vector<vector<int>> mergedMeetings;
-        mergedMeetings.push_back(meetings[0]);
-        for (int i = 1; i < n; i++) {
-            vector<int>& lastMerged = mergedMeetings.back();  // reference to last meeting
-            if (lastMerged[1] >= meetings[i][0]) {
-                lastMerged[1] = max(lastMerged[1], meetings[i][1]);  // check for overlap
+        int noMeetDays = 0;
+        int end = 0;
+        for (auto& meet: meetings) {
+            if (meet[0] > end) {
+                noMeetDays += meet[0] - end - 1;
             }
-            else {
-                mergedMeetings.push_back(meetings[i]);
-            }
+            end = max(meet[1], end);  // merge overlapping meetings
         }
 
-        // calculate days with no meetings
-        int noMeetDays = days;
-        for (auto& interval: mergedMeetings) {
-            noMeetDays -= (interval[1] - interval[0] + 1);
+        // remaining days
+        if (days > end) {
+            noMeetDays += (days - end);
         }
 
         return noMeetDays;
