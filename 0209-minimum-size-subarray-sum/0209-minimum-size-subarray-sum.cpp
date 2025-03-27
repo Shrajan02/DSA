7f30672b0f98a5df442ded34d0683e66c8f205d9
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-03-27 16:25:30 +0530

    Time: 0 ms (100%), Space: 32 MB (72.65%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,43 +1,21 @@
-// Sorting + Binary Search approach
-// TC: O(n * log(n))
+// Sliding Window approach
+// TC: O(n)
 // SC: O(1)
 class Solution {
 public:
-    bool windowFind(int size, vector<int>& nums, int target) {
-        int n = nums.size();
-        int i = 0, j = 0;
-        int sum = 0, maxi = INT_MIN;
-      
-        while (j < n) {
-            sum += nums[j];
-            if (j - i + 1 == size) {
-                maxi = max(sum, maxi);
-                sum -= nums[i];
-                i++;
-            }
-            j++;
-        }
-        if (maxi >= target) {
-            return true;
-        }   
-        return false;
-    }
-
     int minSubArrayLen(int target, vector<int>& nums) {
-        int low = 1, high = nums.size();
-        int minLen = 0;
-        while (low <= high) {
-            int mid = low + ((high - low) >> 1);
-            bool possible = windowFind(mid, nums, target);
-            if (possible) {
-                high = mid - 1;
-                minLen = mid;
-            } 
-            else {
-                low = mid + 1;
+        int n = nums.size();
+        int sum = 0, minLen = INT_MAX;
+        int left = 0;
+        for (int right = 0; right < n; right++) {
+            sum += nums[right];
+            // dynamic window
+            while (sum >= target) {
+                minLen = min(right - left + 1, minLen);
+                sum -= nums[left];
+                left++;
             }
-                
         }
-        return minLen;
+        return (minLen == INT_MAX) ? 0 : minLen;
     }
 };
