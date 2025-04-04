4fa3df77338b73f28b7ed8bfe10ae7e21a9ecf81
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-04-05 03:01:43 +0530

    Time: 0 ms (100%), Space: 21.6 MB (87.02%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cpp
@@ -1,17 +1,20 @@
+// Brute force enumeration approach
+// TC: O(n^3) 
+// SC: O(1)
 #define ll long long 
 class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
         int n = nums.size();
-        ll current = 0, maxi = 0;
+        ll currValue = 0, maxValue = 0;
         for (int i = 0; i < n - 2; i++) {
             for (int j = i + 1; j < n - 1; j++) {
                 for (int k = j + 1; k < n; k++) {
-                    current = (ll)(nums[i] - nums[j]) * nums[k];
-                    maxi = max(current, maxi);
+                    currValue = static_cast<ll>(nums[i] - nums[j]) * nums[k];
+                    maxValue = max(currValue, maxValue);
                 }
             }
         }
-        return maxi;
+        return maxValue;
     }
 };
