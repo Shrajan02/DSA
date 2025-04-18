2d5b6289074080522a6566a3b35ee43a87d4971b
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-04-19 03:06:29 +0530

    Time: 3 ms (35.18%), Space: 16.2 MB (12.21%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
@@ -1,17 +1,21 @@
-// Brute-Force approach
-// TC: O(n^2)
-// SC: O(1)
+// Hash Map approach
+// TC: O(n^2) 
+// SC: O(n)
 class Solution {
 public:
     int countPairs(vector<int>& nums, int k) {
         int n = nums.size();
         int countPairs = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] == nums[j] && (i * j) % k == 0) {
+        unordered_map<int, vector<int>> freqMap;  // {value, indices where value appeared}
+
+        for (int curr = 0; curr < n; curr++) {
+            // check all previous indices with same value
+            for (int prev : freqMap[nums[curr]]) {
+                if ((curr * prev) % k == 0) {
                     countPairs++;
                 }
             }
+            freqMap[nums[curr]].push_back(curr);
         }
         return countPairs;
     }
