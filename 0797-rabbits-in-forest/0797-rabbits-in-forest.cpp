2864b21852116898490e1a57d4e93979e9c27b0e
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-04-21 03:08:26 +0530

    Time: 0 ms (100%), Space: 12.2 MB (70.74%) - LeetHub

---
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
@@ -0,0 +1,30 @@
+// HashMap + Greedy approach
+// TC: O(n)
+// SC: O(n)
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        unordered_map<int, int> freqMap;
+        int minRabbits = 0;
+        for (int ans: answers) {
+            if (ans == 0) {
+                minRabbits++;   // each 0 indicates unique rabbut
+                continue;
+            }
+
+            freqMap[ans]++;
+            // if group complete, add to total & reset
+            if (freqMap[ans] == ans + 1) {
+                freqMap.erase(ans);
+                minRabbits += (ans + 1); 
+            }
+        }
+
+        // handles incomplete groups
+        for (auto& [val, count]: freqMap) {
+            minRabbits += (val + 1);
+        }
+
+        return minRabbits;
+    }
+};
