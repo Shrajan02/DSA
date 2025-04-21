9d2b57d76fd4a1564370a59bc5490e8305bf034f
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-04-22 01:12:26 +0530

    Time: 4 ms (51.92%), Space: 10 MB (45.02%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,32 +1,25 @@
-// Recursive approach
+// Iterative approach
 // TC: O(2^n)
-// SC: O(n)
+// SC: O(2^n)
 class Solution {
 public:
     string countAndSay(int n) {
-        string result = generateString("1", n - 1);
-        return result;
-    }
-
-private:
-    string generateString(const string& base, int n) {
-        if (n == 0) {
-            return base;
-        }
-
-        string res = "";
-        int currCount = 1;
-        for (int i = 1; i < base.length(); i++) {
-            if (base[i] == base[i - 1]) {
-                currCount++;
-            } 
-            else {
-                res += to_string(currCount) + base[i - 1];
-                currCount = 1;
+        string res = "1";
+        for (int i = 1; i < n; i++) {
+            int currCount = 1;
+            string temp = "";
+            for (int j = 1; j < res.size(); j++) {
+                if (res[j] == res[j - 1]) {
+                    currCount++;
+                }
+                else {
+                    temp += to_string(currCount) + res[j - 1];
+                    currCount = 1;
+                }
             }
+            temp += to_string(currCount) + res.back();
+            res = temp;
         }
-        res += to_string(currCount) + base.back(); // handle last group
-
-        return generateString(res, n - 1);
+        return res;
     }
 };
