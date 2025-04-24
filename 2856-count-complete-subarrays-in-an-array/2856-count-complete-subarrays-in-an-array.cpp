1dd327e469abeb77fbf5960d710130a30880ba2f
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-04-25 01:44:11 +0530

    Time: 16 ms (76.37%), Space: 41.8 MB (60.08%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
@@ -1,20 +1,25 @@
-// Brute force approach
-// TC: O(n^2)
+// Sliding Window + Hash Table approach
+// TC: O(n)
 // SC: O(n)
 class Solution {
 public:
     int countCompleteSubarrays(vector<int>& nums) {
         int n = nums.size();
+        // counting distinct elements
         unordered_set<int> unique(nums.begin(), nums.end());
-        int totalUnique = unique.size(), count = 0;
+        int totalUnique = unique.size();
 
-        for (int i = 0; i < n; i++) {
-            unordered_set<int> curr;
-            for (int j = i; j < n; j++) {
-                curr.insert(nums[j]);
-                if (curr.size() == totalUnique) {
-                    count++;
+        int count = 0, start = 0;
+        unordered_map <int, int> freqMap;  // {num, count}
+        for (int end = 0; end < n; end++) {
+            freqMap[nums[end]]++;
+            while (freqMap.size() == totalUnique) {
+                count += (n - end);
+                freqMap[nums[start]]--;
+                if (freqMap[nums[start]] == 0) {
+                    freqMap.erase(nums[start]);
                 }
+                start++;
             }
         }
         return count;
