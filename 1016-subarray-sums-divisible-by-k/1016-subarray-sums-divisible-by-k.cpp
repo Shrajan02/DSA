68b91c080d80f6bba32fe12f50587b6d3199c7f6
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-04-26 02:31:06 +0530

    Time: 13 ms (39.87%), Space: 36.9 MB (6.48%) - LeetHub

---
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,33 @@
+// Prefix Sum + Hash Table approach
+// TC: O(n)
+// SC: O(n)
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count = 0;
+
+        // construct prefix sum array
+        vector<int> prefix(n, 0);
+        prefix[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = nums[i] + prefix[i - 1];
+        }
+
+        // For any subarray nums[i...j], sum: prefix[j] - prefix[i - 1]
+        // (prefix[j] - prefix[i - 1]) % k == 0
+        // (prefix[j] % k) == (prefix[i - 1] % k)
+        // (prefix[i] % k) == (prefix[j] % k)
+        unordered_map<int, int> modCount; // {prefix[i] % k, count}
+        modCount[0] = 1;  // base case
+        for (int i = 0; i < n; i++) {
+            int mod = ((prefix[i] % k) + k) % k; // handle negative mods
+            if (modCount.find(mod) != modCount.end()) {
+                count += modCount[mod]; // found matching mod
+            }
+            modCount[mod]++; 
+        }
+
+        return count;
+    }
+};
