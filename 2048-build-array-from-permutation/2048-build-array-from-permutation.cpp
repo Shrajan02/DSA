0cd9ff55317cd6215605d768f478957b0dd24da6
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-05-27 02:24:18 +0530

    Time: 3 ms (12.04%), Space: 20.4 MB (54.52%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
@@ -1,12 +1,24 @@
+// In Place Transformation approach
+// TC: O(n)
+// SC: O(1)
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
+        // Main problem: How do we keep both the old value and new value in the same array ?!
+        // Solution: Use Base Encoding
         int n = nums.size();
-        vector<int> ans(n);
 
+        // encode both old & new values in the same slot
         for (int i = 0; i < n; i++) {
-            ans[i] = nums[nums[i]];
+            int oldVal = nums[i];  
+            int newVal = nums[nums[i]];
+            nums[i] = oldVal + (newVal % n) * n;  // nums[i] = a + b * n
         }
-        return ans;
+
+        // extract only the new value
+        for (int i = 0; i < n; i++) {
+            nums[i] /= n;
+        }
+        return nums;
     }
 };
