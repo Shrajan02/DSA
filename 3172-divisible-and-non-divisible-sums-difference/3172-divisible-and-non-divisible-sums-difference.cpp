8a27493b16711b724814850b9b42a2e866172f56
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-05-28 03:16:01 +0530

    Time: 0 ms (100%), Space: 8.4 MB (69.71%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
@@ -1,18 +1,16 @@
-// Traversal approach
-// TC: O(n)
+// Math approach
+// TC: O(1)
 // SC: O(1)
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        int diff = 0;
-        for (int num = 1; num <= n; num++) {
-            if (num % m == 0) {
-                diff -= num;
-            }
-            else {
-                diff += num;
-            }
-        }
-        return diff;
+        // diffSums = totalSum - 2 * divisibleSum
+        int totalSum = (n * (n + 1)) / 2;
+        int limit = n / m;
+        // m + 2m + 3m + ... + km
+        // m × (1 + 2 + ... + k)
+        // m × (k × (k + 1)) / 2
+        int divisibleSum_twice = m * (limit * (limit + 1));   
+        return (totalSum - divisibleSum_twice);
     }
 };
