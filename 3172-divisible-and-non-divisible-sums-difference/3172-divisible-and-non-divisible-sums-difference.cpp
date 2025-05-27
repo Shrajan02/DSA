1d1ffd5af361f3da97dc55229177c831ad7b177a
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-05-28 03:17:58 +0530

    Time: 0 ms (100%), Space: 8.4 MB (69.71%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
@@ -6,11 +6,12 @@ public:
     int differenceOfSums(int n, int m) {
         // diffSums = totalSum - 2 * divisibleSum
         int totalSum = (n * (n + 1)) / 2;
-        int limit = n / m;
         // m + 2m + 3m + ... + km
         // m × (1 + 2 + ... + k)
         // m × (k × (k + 1)) / 2
-        int divisibleSum_twice = m * (limit * (limit + 1));   
+        int limit = n / m;
+        int divisibleSum_twice = m * (limit * (limit + 1));  
+        
         return (totalSum - divisibleSum_twice);
     }
 };
