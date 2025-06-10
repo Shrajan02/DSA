39a9b79941d0844193c9a52e746884871192f3c2
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-06-11 02:16:41 +0530

    Time: 0 ms (100%), Space: 9.2 MB (94.34%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -4,15 +4,13 @@
 class Solution {
 public:
     int maxDifference(string s) {
-        int n = s.length();
-        
         // build frequency map
         vector<int> freq(26, 0);
         for (const char& ch: s) {
             freq[ch - 'a']++;
         }
 
-        int maxOdd = 1, minEven = n;
+        int maxOdd = 1, minEven = INT_MAX;
         for (int i = 0; i < 26; i++) {
             // even
             if (freq[i] % 2 == 0 && freq[i] != 0) {
