ccddbb0941aa13de0cb0cd1a01ad4ec2e6c6c3e0
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-06-25 04:14:07 +0530

    Time: 9 ms (21.42%), Space: 15.3 MB (70.04%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -46,8 +46,7 @@ public:
                     visited.insert(node->right);
                 }
                 // parent
-                if (parent_map.count(node) &&
-                    !visited.count(parent_map[node])) {
+                if (parent_map.count(node) && !visited.count(parent_map[node])) {
                     q.push(parent_map[node]);
                     visited.insert(parent_map[node]);
                 }
@@ -60,7 +59,6 @@ public:
             res.push_back(q.front()->val);
             q.pop();
         }
-
         return res;
     }
 
