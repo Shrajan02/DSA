46be2347f952a0c2dc1f7694b99b332239af3192
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-06-27 19:59:50 +0530

    Time: 46 ms (92.68%), Space: 38.1 MB (89.86%) - LeetHub

--- 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.cpp
+++ 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.cpp
@@ -1,38 +1,35 @@
 // Greedy approach
 // TC: O(n)
 // SC: O(1)
-class Solution { 
+class Solution {
 public:
-    int maxDistance(string s, int k) 
-    {
+    int maxDistance(string s, int k) {
         int n = s.length();
         int maxMD = 0;
-        int x = 0, y = 0;   // start at origin
-        
-        for (int i = 0; i < n; i++) 
-        {  
+        int x = 0, y = 0; // start at origin
+
+        for (int i = 0; i < n; i++) {
             char dir = s[i];
-            if (dir == 'N') y++;      
+            if (dir == 'N') y++;
             else if (dir == 'S') y--;
             else if (dir == 'E') x++;
             else if (dir == 'W') x--;
-
-            // calculate potential max distance with up to k changes
+        
             int currMD = abs(x) + abs(y);
-            int steps = i + 1;
-            int wasted = steps - currMD;
+            int steps = i + 1; // potential max distance
+            int wasted = steps - currMD; // steps are wasted when steps != current MD
 
+            // flipping 1 step gives +2 steps
+            // so, flipping k steps give 2*k steps
             int extra = 0;
-            // when steps != currMD
-            if (wasted != 0) {  
-                extra = min(2 * k, wasted);
+            if (wasted != 0) {
+                extra = min(2 * k, wasted);  // {total possible k flips, distance needed to reach steps}
             }
 
             int maxCurrMD = currMD + extra;
             maxMD = max(maxCurrMD, maxMD);
         }
 
-        return maxMD;  
+        return maxMD;
     }
 };
-
