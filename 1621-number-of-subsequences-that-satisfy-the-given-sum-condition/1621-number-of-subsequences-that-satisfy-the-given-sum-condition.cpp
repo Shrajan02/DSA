7655ad1d7f1452105c81baead3be4ccc1e84c512
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-06-30 03:37:59 +0530

    Time: 29 ms (71.12%), Space: 54.3 MB (34.54%) - LeetHub

---
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -0,0 +1,32 @@
+// Sorting + Two Pointers approach
+// TC: O(nlogn)
+// SC: O(n)
+class Solution {
+public:
+    int numSubseq(vector<int>& nums, int target) {
+        const int MOD = 1e9 + 7;
+        int n = nums.size();
+        int count = 0;
+
+        std::sort(nums.begin(), nums.end()); // efficient min-max pairing
+
+        // Precompute power array to handle mod
+        vector<int> pow2(n, 1);   
+        for (int i = 1; i < n; i++) {
+            pow2[i] = (pow2[i - 1] * 2) % MOD;
+        }
+
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            if (nums[left] + nums[right] <= target) {
+                count = (count + pow2[right - left]) % MOD;  // 2^(right - left) acts as bitmask count
+                left++;  // explore greater minimums
+            } 
+            else {
+                right--;  // explore smaller maximums
+            }
+        }
+
+        return count;
+    }
+};
