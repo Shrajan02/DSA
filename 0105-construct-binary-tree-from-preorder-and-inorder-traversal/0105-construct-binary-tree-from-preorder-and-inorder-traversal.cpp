e20795b7616fd56fc6eeac4d23a77373c6f30f97
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-07-03 16:35:11 +0530

    Time: 0 ms (100%), Space: 27.6 MB (20.47%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -6,40 +6,47 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
+
+// Recursive DFS + Hash Table approach
+// TC: O(n)
+// SC: O(n)
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int start = 0, end = preorder.size() - 1;
-        int index = 0;  // maintains preorder position globally
+        int n = preorder.size();
+        int start = 0, end = n - 1;
+        int index = 0; // maintains preorder position globally
 
-        return solve(preorder, inorder, start, end, index);
+        // find splits in O(1)
+        unordered_map<int, int> inorderMap;
+        int i = 0;
+        for (const int& node : inorder) {
+            inorderMap[node] = i++;
+        }
+
+        return solve(preorder, inorder, inorderMap, start, end, index);
     }
 
 private:
-    TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int start, int end, int& index) {
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder, unordered_map<int, int>& inorderMap, int start, int end, int& index) {
         // base case: when range is invalid to search
         if (start > end) {
-            return NULL;   
+            return NULL;
         }
 
         // search the root node in inorder to split left & right subtrees
         int rootVal = preorder[index];
+        int split = inorderMap[rootVal];
 
-        int split = start;
-        while (split <= end) {
-            if (inorder[split] == rootVal) {
-                break;
-            }
-            split++;
-        }
-        index++;  // move to next element of preorder
+        index++; // move to next element of preorder
 
         TreeNode* root = new TreeNode(rootVal);
-        root->left = solve(preorder, inorder, start, split - 1, index);
-        root->right = solve(preorder, inorder, split + 1, end, index);
+        root->left = solve(preorder, inorder, inorderMap, start, split - 1, index);
+        root->right = solve(preorder, inorder, inorderMap, split + 1, end, index);
 
         return root;
     }
