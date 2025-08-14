77071a1f042d6659fc6f96213a360ea1532a32fc
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-08-15 03:17:52 +0530

    Time: 3 ms (53.17%), Space: 15.4 MB (25.83%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
@@ -1,41 +1,28 @@
-// Frequency Sort approach
-// TC: O(n)
+// Priority Queue approach
+// TC: O(n*log(n))
 // SC: O(n)
+using pii = pair<int, int>;
 class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
-        int n = nums.size();
-        std::vector<int> result;
-
-        // construct frequency array
-        std::vector<int> frequency(201, 0);
-        for (int num: nums) {
-            frequency[num + 100]++;
+        std::vector <int> res;
+        
+        std::unordered_map <int,int> freqMap;  
+        for (auto num : nums) {
+            freqMap[num]++;
         }
 
-        // form buckets by frequency
-        std::vector<vector<int>> buckets(n + 1);
-        for (int val = -100; val <= 100; val++) {
-            int count = frequency[val + 100];
-            if (count > 0) {
-                buckets[count].push_back(val);
+        std::priority_queue <pii, vector<pii>, greater<pii>> minH; // {num, freq}
+        for (auto [num, freq] : freqMap) {
+            minH.push({freq, -num}); // using -ve to handle descending order for same frequency
+        }
+        while (!minH.empty()) {
+            for (int i = 0; i < minH.top().first; i++) { // freq 
+                res.push_back(-minH.top().second); // num 
             }
-        } 
-
-        // process buckets from high to low frequency
-        for (int i = 1; i <= n; i++) {
-            vector<int> temp = buckets[i];
-            int bucket_size = temp.size();
-            
-            // already in descending order from 100 to -100
-            for (int j = bucket_size - 1; j >= 0; j--) {
-                int counter = i;
-                while (counter--) {
-                    result.push_back(temp[j]);
-                }
-            }
+            minH.pop();
         }
 
-        return result;
+        return res;
     }
 };
