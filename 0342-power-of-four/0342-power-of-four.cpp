bbfe2f0bc500a0c92c12571eb63e5fc9fe2b415e
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-08-17 01:25:51 +0530

    Time: 0 ms (100%), Space: 7.6 MB (98.24%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,10 +1,13 @@
-// Math approach (inbuilt functions)
+// Bit Manipulation approach
 // TC: O(1)
 // SC: O(1)
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        double remainder = std::log10(n) / std::log10(4);
-        return (n > 0) && std::fmod(remainder, 1) == 0; 
+        if (n < 0) return false;
+
+        // check power of 2 & whether the MSB is set at even bit position
+        int mask = n - 1;
+        return ((n & mask) == 0) && (mask % 3 == 0);
     }
 };
