ab30eb96e11c1f04c4cafb0b0c45b7dc8e8602d0
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2025-08-28 23:18:09 +0530

    Time: 0 ms (100%), Space: 22.9 MB (65.22%) - LeetHub

--- 3321-type-of-triangle/3321-type-of-triangle.cpp
+++ 3321-type-of-triangle/3321-type-of-triangle.cpp
@@ -1,11 +1,11 @@
 // Basic Maths Approach
-// TC: O(1)
+// TC: O(nlogn)
 // SC: O(1)
 class Solution {
 public:
     string triangleType(vector<int>& nums) {
         // TRIANGLE INEQUALITY THEOREM: Sum of any two sides must be greater than the third
-        sort(nums.begin(), nums.end());  // After sorting, sum of the two smallest sides must be greater than the largest to form a valid triangle
+        std::sort(nums.begin(), nums.end());  // After sorting, sum of the two smallest sides must be greater than the largest to form a valid triangle
         if (nums[0] + nums[1] <= nums[2]) {
             return "none";
         }
