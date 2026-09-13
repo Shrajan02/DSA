1715e5b35b5b7966c3f3a649c2ed9c0a8017270d
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2026-09-14 02:13:05 +0530

    Time: 0 ms (100%), Space: 17.4 MB (79.75%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -0,0 +1,38 @@
+// DFS approach
+// TC: O(V + E)
+// SC: O(V)
+// bipartite -> if you can color every node with 2 colors such that adjacent nodes have the same color
+// odd-length cycles are NEVER bipartite, rest all are bipartite
+class Solution {
+private:
+    bool dfs(const vector<vector<int>>& graph, vector<int>& colors, int current_node, int current_color) {
+        colors[current_node] = current_color;
+
+        for (int neighbor: graph[current_node]) {
+            if (colors[neighbor] == current_color) {
+                return false;
+            }
+
+            int reverse_color = 1 - current_color;
+            if (colors[neighbor] == -1 && !dfs(graph, colors, neighbor, reverse_color)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> colors(n, -1);  // -1 -> no color, 0 -> one color, 1 -> other color
+        
+        for (int v = 0; v < n; v++) {
+            if (colors[v] == -1 && !dfs(graph, colors, v, 0)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
