0fc63f0f98868c45f1ae694360e2f9ebe6226372
Shrajan Karkera <109146806+Shrajan02@users.noreply.github.com>
2026-09-19 00:15:06 +0530

    Time: 0 ms (100%), Space: 15.7 MB (54.18%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,20 +1,31 @@
+// 2-pointers approach
+// TC: O(N)
+// SC: O(1)
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size(), index = -1;
-        for (int i = n-2; i >= 0; i--) {
-            if (nums[i] < nums[i+1]) {
-                index = i;
+        int n = nums.size();
+        int pivot = -1;
+
+        // find the first decreasing element from the right
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                pivot = i;
                 break;
             }
         }
 
-        for (int i = n-1; i >= index && index != -1; i--) {
-            if (nums[i] > nums[index]) {
-                swap(nums[i], nums[index]);
-                break;
+        // find the smallest element larger than nums[pivot] from the right
+        if (pivot != -1) {
+            for (int i = n - 1; i > pivot; i--) {
+                if (nums[i] > nums[pivot]) {
+                    std::swap(nums[i], nums[pivot]);
+                    break;
+                }
             }
         }
-        reverse(nums.begin() + index + 1, nums.end());
+
+        // reverse elements after the pivot
+        std::reverse(nums.begin() + pivot + 1, nums.end());
     }
 };
